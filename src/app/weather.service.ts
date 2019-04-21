ced658ed29fe05c06a03e8c97c6978cdf698be45
Mohamedaquib Chiniwala <mohamedaquib.chiniwala@nagarro.com>
2019-04-21 12:08:41 +0530

    production tweaks

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -6,10 +6,10 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   fetchCurrentWeather(city: string) {
-    return this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=8a74dfe57833cb92302a7776463a6b08&units=metric`);
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=8a74dfe57833cb92302a7776463a6b08&units=metric`);
   }
 
   fetchForecast(city: string) {
-    return this.http.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=8a74dfe57833cb92302a7776463a6b08&units=metric`);
+    return this.http.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=8a74dfe57833cb92302a7776463a6b08&units=metric`);
   }
 }
